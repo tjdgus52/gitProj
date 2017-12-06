fa94cdb79022d9ea3d4bf3a36f31869274a0c6ee
java <java@java-PC>
2017-12-06 16:16:17 +0900

    homecontroller 2차 수정

--- src/main/java/com/hy/sptest/HomeController.java
+++ src/main/java/com/hy/sptest/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		System.out.println("test");
+		System.out.println("test22");
 		
 		model.addAttribute("serverTime", formattedDate );
 		
